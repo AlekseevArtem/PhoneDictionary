7bde0989f877d4ba7b6102a8d76c9a56af08442b
AlekseevArtem <tema9991@mail.ru>
2020-06-24 19:32:56 +0300

    2. Чтение записей из телефонной книги. [#294998]

--- app/src/main/java/ru/job4j/phonedictionary/MainActivity.java
+++ app/src/main/java/ru/job4j/phonedictionary/MainActivity.java
@@ -1,13 +1,147 @@
 package ru.job4j.phonedictionary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter<RecyclerView.ViewHolder> adapter;
+    private List<String> phones = new ArrayList<>();
+    private int PERMISSION_REQUEST_CODE = 12;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recycler = findViewById(R.id.phones);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        phones = new ArrayList<>();
+        adapter = new PhoneAdapter(phones);
+        recycler.setAdapter(adapter);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.READ_CONTACTS}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity, menu);
+        MenuItem menuitem = menu.findItem(R.id.search_icon);
+        SearchView searchView = (SearchView) menuitem.getActionView();
+        searchView.setQueryHint("Search here");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                phones.clear();
+                Cursor cursor = getContentResolver().query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        new String[] {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                                ContactsContract.CommonDataKinds.Phone.NUMBER},
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " like '%" + newText + "%'",
+                        null,
+                        null);
+                try {
+                    while (cursor.moveToNext()) {
+                        String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                        String phone = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        phones.add(name + " " + phone);
+                    }
+                    adapter.notifyDataSetChanged();
+                } finally {
+                    cursor.close();
+                }
+                return true;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            for (int i = 0; i < permissions.length; i++) {
+                String permission = permissions[i];
+                int grantResult = grantResults[i];
+
+                if (permission.equals(android.Manifest.permission.READ_CONTACTS)) {
+                    if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                        loadDic(phones);
+                    }
+                }
+            }
+        }
+    }
+
+    private void loadDic(List<String> phones) {
+        Cursor cursor = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[] {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Phone.NUMBER},
+                null,
+                null, null);
+        try {
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phone = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                phones.add(name + " " + phone);
+            }
+            adapter.notifyDataSetChanged();
+        } finally {
+            cursor.close();
+        }
+    }
+
+    public static final class PhoneAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private final List<String> phones;
+
+        public PhoneAdapter(List<String> phones) {
+            this.phones = phones;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new RecyclerView.ViewHolder(
+                    LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.phone, parent, false)
+            ) {};
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            TextView text  = holder.itemView.findViewById(R.id.name);
+            text.setText(phones.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return phones.size();
+        }
     }
 }
